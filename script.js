a3d943b64eb57f8c744d59c05dc77c191432bfee
bilisimschool <schoolbilisim2@gmail.com>
2025-04-05 19:24:55 +0200

    changes

---
+++ script.js
@@ -0,0 +1,5 @@
+// Optional JavaScript functionality here
+
+// Example: Smooth scrolling is natively handled by the 'scroll-behavior: smooth;' CSS property.
+// If you want more advanced scrolling or animations, you can add your code here.
+console.log("Portfolio site is loaded and ready!");
